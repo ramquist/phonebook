dcc8ee9966b4bdcca88996b6b71b0b0edb513d0f
Victoria Melnikova <31312528+ramquist@users.noreply.github.com>
2019-06-25 23:48:33 +0300

    menu in an endless loop

--- phonebook.py
+++ phonebook.py
@@ -4,9 +4,9 @@
 import os
 
 class Phonebook:
-    def __init__(self): #конструктор класса
+    def __init__(self): 
         self.phonebook = {}
-        #Запрашивать у пользователя имя файла
+        # Prompt name of file
         file_name = input("""\
 WELCOME TO THE PROGRAM 'THE PHONEBOOK'.
 NOW OPEN AN EXISTING BOOK OR CREATE A NEW ONE.
@@ -20,10 +20,10 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
             print("PHONEBOOK CREATED SUCCESSFULLY")
 
     def loadAll(self):
-        #Clear the phonebook dictionary
+        # Clear the phonebook dictionary
         self.phonebook.clear()
         
-        #Load all of the items from the text file into the dictionary
+        # Load all of the items from the text file into the dictionary
         file = open(self.phonebook_file, 'r')
         for line in file.readlines():
             name, number = line.strip().split()
@@ -31,11 +31,11 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
         file.close()
 
     def openFile(self):
-        #Check if the file exists on your computer and create the file if it does not exist
+        # Check if the file exists on your computer and create the file if it does not exist
         file_name = input("ENTER THE FILE NAME WITHOUT THE EXTENSION: ")
         self.phonebook_file = file_name + '.txt'
         if os.path.exists(self.phonebook_file): 
-            print("PHONEBOOK ALREADY EXISTS")
+            print("PHONEBOOK ALREADY EXISTS AND IS OPEN")
             return
         else:
             file = open(self.phonebook_file, 'w')
@@ -43,16 +43,15 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
         
     def addContact(self):
         self.loadAll()
-        #Prompt the user for the details of the new entry
+        # Prompt the user for the details of the new entry
         name = input("ENTER NAME: ")
         number =input("ENTER NUMBER: ")
-        if not name or number:
+        if (name == "" or number == ""):
             print("EMPTY STRING. NEW CONTACT IS NOT CREATED")
-            return
-       
-        #Создать строку для записи в файл
+
+        # Create a string to be written to the file
         new_entry = name + '\t' + number + '\n'
-        #Записать строку в файл
+        # Write the string to the file
         file = open(self.phonebook_file, 'a')
         file.write(new_entry)
         file.close()
@@ -60,7 +59,7 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
         
     def readAll(self):
         self.loadAll()
-        #Распечатать весь словарь телефонной книги
+        # Print out the entire phonebook dictionary
         for name, number in self.phonebook.items():
             print(name, " : ", number)
         if len(self.phonebook) == 0:
@@ -68,7 +67,7 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
             
     def searchContact(self):
         self.loadAll()
-        #Prompt the user for the name to search for, and search the phonebook dictionary
+        # Prompt the user for the name to search for, and search the phonebook dictionary
         search = input("ENTER NAME TO SEARCH FOR: ")
         if search in self.phonebook.keys():
             print(search, " : ", self.phonebook[search])
@@ -91,8 +90,10 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
 
     def exitProgram(self):
         os._exit
+
     def menu(self):
-        print("""\
+        while True:
+            print("""\
        -МЕНЮ-
 1) OPEN THE PHONEBOOK
 2) READ ALL CONTACTS
@@ -100,17 +101,19 @@ ENTER THE FILE NAME WITHOUT THE EXTENSION: """)
 4) DELETE AN CONTACT
 5) LOOK UP AN CONTACT
 6) EXIT\n""")
-        choice = input("ENTER CHOICE: ")
-        choice_menu = {'1' : self.openFile,
-                       '2' : self.readAll,
-                       '3' : self.addContact,
-                       '4' : self.deleteContact,
-                       '5' : self.searchContact,
-                       '6' : self.exitProgram}
-        if choice not in choice_menu.keys():
-            print("PLEASE ENTER A VALID CHOICE")
-        else:
-            choice_menu[choice]()
+            choice = input("ENTER CHOICE: ")
+            choice_menu = {'1' : self.openFile,
+                           '2' : self.readAll,
+                           '3' : self.addContact,
+                           '4' : self.deleteContact,
+                           '5' : self.searchContact,
+                           '6' : self.exitProgram}
+            if choice not in choice_menu.keys():
+                print("PLEASE ENTER A VALID CHOICE")
+            elif choice == '6':
+                break
+            else:
+                choice_menu[choice]()
             
 Book_1 = Phonebook()
 Book_1.menu()
